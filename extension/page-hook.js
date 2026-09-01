93f6a884dea25e4bd934abeea88f55b6dd610678
Minashin1120 <Minashin1120@users.noreply.github.com>
2026-09-02 00:57:43 +0900

    v1.6.1: bump version for consent-page fix

--- extension/page-hook.js
+++ extension/page-hook.js
@@ -1525,7 +1525,7 @@
   }
 
   function boot() {
-    logInfo("boot", { href: location.href, ua: navigator.userAgent, v: "1.6.0" });
+    logInfo("boot", { href: location.href, ua: navigator.userAgent, v: "1.6.1" });
     patchFetch();
     patchXHR();
     watchDom();
